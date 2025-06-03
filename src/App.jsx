ec907d80156b290670e43230f23f1a87266dba2c
PatilSakshiB <patilsakshi1228@gmail.com>
2025-06-03 18:13:42 +0530

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Components/Navbar'
+import News from './Components/News'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Navbar/>
+     <News/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/News.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NewsItem from './NewsItem'
+
+export default function () {
+  return (
+    <div> 
+        <h4>News Component</h4>
+        <NewsItem/>
+    </div>
+  )
+}
