cd65d1d219e4545d86ef9d74ec60dd2ad98ae1d3
PatilSakshiB <patilsakshi1228@gmail.com>
2025-06-04 22:27:37 +0530

    Add News Heading structure

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -4,8 +4,33 @@ import NewsItem from './NewsItem'
 export default function () {
   return (
     <div> 
-        <h4>News Component</h4>
-        <NewsItem/>
+      <div style={{border:'1px solid'}}>
+        <h3>News Component</h3>
+        <div className="container">
+          <div className="row my-3">
+            <div className="col-md-4">
+              <NewsItem title='Headline 1'/>
+            </div>
+             <div className="col-md-4">
+              <NewsItem title='Headline 2'/>
+            </div>
+             <div className="col-md-4">
+              <NewsItem title='Headline 3'/>
+            </div>
+          </div>
+          <div className="row my-3">
+            <div className="col-md-4">
+              <NewsItem/>
+            </div>
+             <div className="col-md-4">
+              <NewsItem/>
+            </div>
+             <div className="col-md-4">
+              <NewsItem/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function NewsItem() {
-  return (
-    <div>
-        <h5>NewsItem Component</h5>
+export class NewsItem extends Component {
+  render() {
+    let {title,description} = this.props;
+    return (
+       <div>
+        <div className="card" style={{width: "18rem"}}>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+          </div>
+        </div>
     </div>
-  )
+    )
+  }
 }
+
+export default NewsItem
